c0b590feaa3f89e0f8eb945f81b0448dff35a7cd
Web Developer <web.newdiscoverytech@gmail.com>
2021-11-01 15:30:09 -0600

    [ADD]: getData from API, design table, filterSearch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { TableCoins } from './components/TableCoins';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const getData = async () => {
+        const res = await axios.get(
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1'
+        );
+        setCoins(res.data);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div className='container mt-5 mb-5'>
+            <div className='row'>
+              <h1 className="text-center">Top 100 cryptocurrencies</h1>
+                <input
+                    type='text'
+                    placeholder='Search a coin'
+                    className='form-control bg-dark text-light border-0 mt-4 text-center'
+                    onChange={ e => setSearch(e.target.value) }
+                />
+                <TableCoins coins={coins} search={search} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
